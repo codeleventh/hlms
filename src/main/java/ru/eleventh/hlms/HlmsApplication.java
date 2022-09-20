14af3f66f377c34bcd14ec0045e37334093f4b71
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2022-09-20 19:27:22 +0400

    init()

---
+++ src/main/java/ru/eleventh/hlms/HlmsApplication.java
@@ -0,0 +1,13 @@
+package ru.eleventh.hlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/eleventh/hlms/HlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eleventh.hlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
